d1b66917180112d113b3ea784ae366240c04f784
lehui123 <65656404+lehui123@users.noreply.github.com>
2021-10-03 23:22:47 +0800

    install,setup and auth firebase

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCT6D9uGl4OQ7d-olXZ06B8OvFhiK8jGK0",
     authDomain: "hearty-2f281.firebaseapp.com",
@@ -6,3 +10,14 @@ const firebaseConfig = {
     messagingSenderId: "818441956657",
     appId: "1:818441956657:web:3cf6693e157740d0aa1410"
   };
+
+  let app;
+  if (firebase.apps.length === 0) {
+    app = firebase.intializeApp(firebaseConfig);
+  } else {
+    app = firebase.app();
+  }
+  const db = app.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth };

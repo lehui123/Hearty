80c97a49761c22072e0b6305ba14df67065b7f3c
lehui123 <65656404+lehui123@users.noreply.github.com>
2021-10-15 13:47:26 +0800

    Login and logout function

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
-import { auth } from '../../firebase';
-
-export default function Home() { 
+import { auth } from '../../config';
+import AppButton from '../../components/AppButton';
+export default function Home({navigation}) { 
+    const Logout = () => {
+        auth
+        .signOut()
+        .then(()=>{
+        navigation.navigate('Login');
+        })
+        .catch((error) => alert(error.message));  
+    }
     return (
         <View>
             <Text>Welcome {auth?.currentUser?.displayName}</Text>
+            <AppButton onPress={Logout} title = "Log out"/>
         </View>
     );
 }

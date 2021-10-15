414382abab784aeff588d9a97ba1568397d66c6f
ZiJiaaaa <zjchong24@gmail.com>
2021-10-15 19:49:37 +0800

    Changes: Home, Measurement, Information, Profile

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
 import styles from './styles';
 
-export default function Profile() {
+const username = 'Xiaoming'
+const gender = 'female'
+const age = 25
+const lastbpm = 139
+const bpmHistory = 'Day1: 130'
+
+function Profile (props) {
     return (
-        <View style = {styles.backgroundContainer}>
-            <Text style = {styles.textColor}>Hello this is the profile page hahaha</Text>
-        </View>
+        <SafeAreaView style = {styles.styleContainer}>
+            <SafeAreaView style = {styles.innerContainer}>
+            <Text style = {styles.header}>User Profile</Text>
+            </SafeAreaView>
+            <SafeAreaView style = {styles.body}>
+            <Text style = {styles.box}>Name: {username} </Text>
+            </SafeAreaView>
+            <SafeAreaView style = {styles.body}>
+            <Text style = {styles.box}>Gender: {gender} </Text>
+            </SafeAreaView>
+            <SafeAreaView style = {styles.body}>
+            <Text style = {styles.box}>Age: {age} </Text>
+            </SafeAreaView>
+            <SafeAreaView style = {styles.body}>
+            <Text style = {styles.box}>Last BPM: {lastbpm} </Text>
+            </SafeAreaView>
+            <SafeAreaView style = {styles.innerContainer}>
+            <Text style = {styles.header}>BPM History</Text>
+            </SafeAreaView>
+            <SafeAreaView style = {styles.fillbackground}>
+            <Text style = {styles.box}> {bpmHistory} </Text>
+            </SafeAreaView>
+        </SafeAreaView>
+    
     );
 }
 
+export default Profile

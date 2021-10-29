5c89ce311dbe3a7fa3ce99ff634c9acba169fdb3
lehui123 <65656404+lehui123@users.noreply.github.com>
2021-10-29 15:06:47 +0800

    history

--- screens/History/History.js
+++ screens/History/History.js
@@ -3,9 +3,10 @@ import {View, TouchableOpacity,Text,FlatList,StyleSheet, SectionList} from 'reac
 import AppButton from '../../components/AppButton';
 
 import { auth, db } from '../../config';
+import styles from '../History/styles';
 
 export default function History({navigation}) {
-    const [hist,setHist] = useState([])
+    //const [hist,setHist] = useState([])
     const [History, setHistory] = useState([])
     
     useEffect(() => {
@@ -13,32 +14,34 @@ export default function History({navigation}) {
     .ref('/users/'+auth.currentUser.uid+'/records')
     .once('value')
     .then(snapshot => {
-        setHist(snapshot.val())
+        // setHist(snapshot.val())
+        let history = [];
+    
+        for (const [key, value] of Object.entries(snapshot.val())) { 
+    
+            const alltime = [];
+    
+            for (const[ky,val] of Object.entries(value)){
+                const vl = '';
+                
+                for (const[k,v] of Object.entries(val)){
+                    vl = v;
+                    
+                }
+                const obj2 = {'time': ky, 'BPM': vl}
+                alltime.push(obj2)
+            }          
+                
+            const obj1 = {'date': key, 'data': alltime}
+            history.push(obj1)
+    
+        }
+        setHistory(history)
     })
     }, []);
 
-    let history = [];
-    
-    for (const [key, value] of Object.entries(hist)) { 
 
-        const alltime = [];
 
-        for (const[ky,val] of Object.entries(value)){
-            const vl = '';
-            
-            for (const[k,v] of Object.entries(val)){
-                vl = v;
-                
-            }
-            const obj2 = {'time': ky, 'BPM': vl}
-            alltime.push(obj2)
-        }          
-            
-        const obj1 = {'date': key, 'records': alltime}
-        history.push(obj1)
-
-    }
-    setHistory(history)
 
 
     return (
@@ -53,34 +56,28 @@ export default function History({navigation}) {
         //         </div>  )
         //     ))}
         // </div>
+        <View style = {styles.container}>
+        <Text style = {styles.titleHeader}> History </Text>
+        <View style = {styles.infoContainer}>
         <SectionList
         sections={History}
-        renderItem={({item}) => <Text>{item}</Text>}
-        renderSectionHeader={({section}) => <Text >{section.date}</Text>}
+        renderItem={({item}) => <Text style = {styles.itemHeader}>{item.time} : {item.BPM}</Text>}
+        renderSectionHeader={({section}) => <Text style = {styles.dateHeader}>{section.date}</Text>}
         keyExtractor={(item, index) => index}
         />
+        </View>
+        <View style = {{justifyContent: 'center'}}>
+        <TouchableOpacity
+        style={styles.styleButton}
+        onPress={() =>{navigation.navigate('Profile Page')}}
+        >
+        <Text style={styles.styleButtonText}>Profile</Text>
+        </TouchableOpacity>
+        </View>
+        </View>
 
-        
+
 
     )
     
  }
-const styles = StyleSheet.create({
-
-    container: {
-      marginTop: 20,
-      flex: 1,
-    },
-    row: {
-    
-      padding: 15,
-      marginBottom: 5,
-      backgroundColor: 'skyblue',
-      flexDirection: 'column',
- 
-    },
-    textformat:{
-        color: '#000000',
-        fontSize: 10,
-    }
-  })
--- screens/History/styles.js
+++ screens/History/styles.js
@@ -1,3 +1,73 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, StatusBar, Platform } from 'react-native'
 import Colour from '../../components/Colour'
 import Constants from "expo-constants";
+import { Dimensions } from 'react-native';
+
+
+const WIDTH = Math.round(Dimensions.get('window').width)
+const HEIGHT = Math.round(Dimensions.get('window').height)
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent: 'center',
+        //alignContent: 'center',
+        alignItems: "center",
+        padding: 20,
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: Colour.primary
+    },
+    infoContainer:{
+        paddingTop: 20,
+        alignContent: 'center',
+        alignItems: "center",
+        padding: 20,
+        width:Dimensions.get("window").width * 0.80,
+        height: HEIGHT * 0.7,
+        backgroundColor: Colour.secondary,
+        marginBottom: 30,
+    },
+    titleHeader: {
+        fontSize:25,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: Colour.brand,
+        paddingTop: 10,
+
+    },
+    itemHeader: {
+        fontSize: 15,
+        color: Colour.tertiary,
+        paddingLeft: 10,
+   
+
+    },
+    dateHeader: {
+        padding: 10,
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: Colour.tertiary
+    },
+    styleButton:{
+        // marginTop: HEIGHT/25,
+        backgroundColor:Colour.tertiary,
+        // width:200,
+        // height:60,
+        // borderRadius: 20,
+        width:Dimensions.get("window").width * 0.35,
+        height:Dimensions.get("window").height * 0.10,
+        borderRadius: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+       // alignContent: 'center'
+
+    },
+    styleButtonText:{
+        color: Colour.primary,
+        fontSize: 18,
+        textTransform: "uppercase",
+        fontWeight: "bold",
+        textAlign: "center",
+
+    }
+})
+export default styles;
